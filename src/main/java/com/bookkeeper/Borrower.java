8fdd2839c0196d2550105e63cb1b4567478f1898
epasper@gmail.com <epasper@gmail.com>
2020-03-27 13:41:08 +0100

    Added Book Borrowers as entity

--- src/main/java/com/bookkeeper/Borrower.java
+++ src/main/java/com/bookkeeper/Borrower.java
@@ -11,11 +11,6 @@ public class Borrower extends User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     int borrowerId;
-    String username;
-    String firstName;
-    String surname;
-    String email;
-    String password;
     @OneToMany(
             mappedBy="borrower",
             fetch = FetchType.EAGER,
@@ -30,56 +25,6 @@ public class Borrower extends User {
 
     public void setBorrowerId(int borrowerId) {
         this.borrowerId = borrowerId;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
-    public String getSurname() {
-        return surname;
-    }
-
-    @Override
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public List<Book> getBorrowedBooks() {
